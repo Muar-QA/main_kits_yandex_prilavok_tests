81e525bc3f3cc3ed63b6826b6dc734252a81626f
Muar-QA <snv7982@yandex.ru>
2026-09-16 20:45:08 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -5,8 +5,5 @@ URL_SERVICE = "https://dee2abb0-9880-4d7f-b68c-93748c564a4c.serverhub.praktikum-
 # Путь для создания нового пользователя
 CREATE_USER_PATH = "/api/v1/users/"
 
-# Путь для получения таблицы пользователей (чтобы проверить, создался ли пользователь)
-USERS_TABLE_PATH = "/api/v1/users/"
-
 # Путь для создания нового набора (kit)
 CREATE_KIT_PATH = "/api/v1/kits/"
